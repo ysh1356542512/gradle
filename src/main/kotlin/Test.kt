f6044b041d6670a2b29aa2a0a2600cb940c2819b
Master Leaf <ysh1356542512@163.com>
2022-03-20 21:31:42 +0800

    [update]完结gradle启动 开启daemon构建的源码分析

--- src/main/kotlin/Test.kt
+++ src/main/kotlin/Test.kt
@@ -1,5 +1,9 @@
+import org.gradle.launcher.GradleMain
+import org.gradle.launcher.daemon.bootstrap.GradleDaemon
 import org.gradle.wrapper.GradleWrapperMain
 
 fun main(args: Array<String>) {
-     GradleWrapperMain.main(args)
+//     GradleWrapperMain.main(args)
+//     GradleMain.main(args)
+     GradleDaemon.main(args)
 }
